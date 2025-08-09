c5ebefba25d1e170f5c5cb93752656b66514ff62
jpbisciotti <76615346+jpbisciotti@users.noreply.github.com>
2025-08-09 13:56:10 -0400

    Create feat_rm_nzv.R

---
+++ feat_rm_nzv.R
@@ -0,0 +1,49 @@
+feat_rm_nzv <- function(
+    x,
+    max_frequency_ratio = 19,
+    max_uniqueness_ratio = 10
+) {
+  
+  is.nzv <- function(
+    x, 
+    max_frequency_ratio, 
+    max_uniqueness_ratio
+  ) {
+    
+    tb <- table(x)
+    
+    first_mode <- max(tb)
+    second_mode <- max(tb[tb != max(tb)])
+    frequency_ratio <- first_mode / second_mode
+    excessive_frequency_ratio <- frequency_ratio > max_frequency_ratio
+    
+    n_unique <- length(tb)
+    n_total <- sum(tb)
+    uniqueness_ratio <- n_total / n_unique
+    excessive_uniqueness_ratio <- uniqueness_ratio > max_uniqueness_ratio
+    
+    excessive_frequency_ratio && excessive_uniqueness_ratio
+  }
+  
+  df |> 
+    dplyr::select(-tidyselect::where(function(x) { 
+      is.nzv(
+        x = x, 
+        max_frequency_ratio = max_frequency_ratio, 
+        max_uniqueness_ratio = max_uniqueness_ratio
+      ) 
+    } ))
+  
+}
+
+if (FALSE) {
+  set.seed(123)
+  df <- tibble::tibble(
+    a = round(rnorm(100 * 5), 2), # FALSE TRUE
+    b = c(rep(1, 95 * 5), rep(2, 5 * 5)),  # FALSE FALSE
+    c = c(rep(1, 96 * 5), rep(2, 4 * 5)) # TRUE TRUE
+  )
+  df |> feat_rm_nzv() 
+}
+
+
